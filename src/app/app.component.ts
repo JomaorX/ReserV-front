552cbf3c55bbc9ce87ede5cfb344fcd4d31e9c4e
Jose Manuel Garcia Lopez <jmgl.dev@gmail.com>
2025-05-18 22:03:47 +0200

    🔥 Primer commit del ReserV frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'reserv-frontend';
+  esLoginPage = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects;
+        this.esLoginPage = ['/login', '/register'].includes(url);
+      }
+    });
+  }
 }
